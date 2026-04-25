fef4f0b1dd55bcd98088ab534359eeb019a1a5fd
Jinwiil Onginjo <onginjokelvin31@gmail.com>
2026-04-26 01:57:50 +0300

    Update general.js

--- gifted/general.js
+++ gifted/general.js
@@ -43,43 +43,10 @@ gmd(
 
     await sendButtons(Gifted, from, {
 //=======================================================//
-function formatUptime(seconds) {
-        const days = Math.floor(seconds / (24 * 60 * 60));
-        seconds %= 24 * 60 * 60;
-        const hours = Math.floor(seconds / (60 * 60));
-        seconds %= 60 * 60;
-        const minutes = Math.floor(seconds / 60);
-        seconds = Math.floor(seconds % 60);
-        return `${days}d ${hours}h ${minutes}m ${seconds}s`;
-      }
-
-      const now = new Date();
-      const date = new Intl.DateTimeFormat("en-GB", {
-        timeZone: timeZone,
-        day: "2-digit",
-        month: "2-digit",
-        year: "numeric",
-      }).format(now);
-
-      const time = new Intl.DateTimeFormat("en-GB", {
-        timeZone: timeZone,
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        hour12: true,
-      }).format(now);
-
-      const uptime = formatUptime(process.uptime());
-      const totalCommands = commands.filter(
-        (command) => command.pattern && !command.dontAddCommandList,
-      ).length;
-      //==========================================//
+//=================================//
       
       title: "Bot Speed",
       text: `☄️ Latency: ${responseTime}ms`,
-      Dᴀᴛᴇ :`${monospace(date)}`,
-      Tɪᴍᴇ:` ${monospace(time)}`,
-      Uᴘᴛɪᴍᴇ :`${monospace(uptime)}`,
       footer: `> *${botFooter}*`,
       buttons: [
         { id: `${botPrefix}uptime`, text: "⏱️ Uptime" },
