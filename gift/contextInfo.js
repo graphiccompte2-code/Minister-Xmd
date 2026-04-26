ed61118586239ff59c91fc3ea4132de284613e7c
Jinwiil Onginjo <onginjokelvin31@gmail.com>
2026-04-26 04:03:18 +0300

    Fix bot name fallback in contextInfo.js

--- gift/contextInfo.js
+++ gift/contextInfo.js
@@ -1,7 +1,7 @@
 const { getSetting } = require("./database/settings");
 
 async function getContextInfo(mentionedJid = []) {
-    const botName = await getSetting("BOT_NAME") || "𝐌𝐢𝐧𝐢𝐬𝐭𝐞𝐫-𝐗𝐦𝐝";
+    const botName = await getSetting("BOT_NAME") || "𝐌𝐢𝐧𝐢𝐬𝐭𝐞𝐫-𝐗𝐝";
     const channelJid = await getSetting("NEWSLETTER_JID") || "120363325383451197@newsletter";
     return {
         mentionedJid,
